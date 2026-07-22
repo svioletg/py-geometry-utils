3da1af8219a7d65039b97a7856d060117625f9d8
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-07-21 20:22:45 -0500

    refactor: access x and y attributes of self in Coord2.in_bounds(), add examples to docstring

--- geometry/__init__.py
+++ geometry/__init__.py
@@ -182,8 +182,13 @@ class Coord2:
         return s.format(x=self.x, y=self.y)
 
     def in_bounds(self, rect: RectOrTuple) -> bool:
-        """Returns whether this coordinate is within a rectangle's bounds."""
-        return (rect[0] < self[0] < rect[2]) and (rect[1] < self[1] < rect[3])
+        """Returns whether this coordinate is within a rectangle's bounds, not counting the edge.
+
+        >>> assert Coord2(1, 1).in_bounds((0, 0, 2, 2))
+        >>> assert not Coord2(0, 0).in_bounds((0, 0, 2, 2))
+        >>> assert not Coord2(2, 2).in_bounds((0, 0, 2, 2))
+        """
+        return (rect[0] < self.x < rect[2]) and (rect[1] < self.y < rect[3])
 
     def map(self, fn: Callable[[float], float]) -> Self:
         """Returns a new instance of this class with ``fn`` applied to its ``x`` and ``y`` attributes."""
