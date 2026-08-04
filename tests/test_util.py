3ec479db460b7257653a00599d8c1e4cd8996362
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-08-04 14:49:37 -0500

    feat: add function `util.lerp()`

--- tests/test_util.py
+++ tests/test_util.py
@@ -2,7 +2,7 @@ import math
 
 import pytest
 
-from geometry.util import ident, partition, snap_num, take_n
+from geometry.util import ident, lerp, partition, snap_num, take_n
 
 
 def test_ident() -> None:
@@ -14,6 +14,16 @@ def test_snap_num() -> None:
     assert snap_num(6, 10, round) == 10  # noqa: PLR2004
     assert snap_num(6, 10, math.ceil) == 10  # noqa: PLR2004
     assert snap_num(6, 10, math.floor) == 0
+
+def test_lerp() -> None:
+    assert lerp(0, 1, 0) == 0
+    assert lerp(0, 1, 1) == 1
+    assert lerp(0, 1, 0.5) == 0.5  # noqa: PLR2004
+    assert lerp(0, -1, 0.5) == -0.5  # noqa: PLR2004
+    assert lerp(1, 3, 0) == 1
+    assert lerp(1, 3, 2) == 5  # noqa: PLR2004
+    assert lerp(1, 3, 0.5) == 2  # noqa: PLR2004
+    assert lerp(-1, -3, 0.5) == -2  # noqa: PLR2004
 
 def test_partition() -> None:
     assert partition(range(10), lambda x: x % 2 == 0) == ([0, 2, 4, 6, 8], [1, 3, 5, 7, 9])
