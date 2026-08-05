ba6dcc5d3b8d23c469ce2977a763669467682d0f
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2026-08-05 14:23:26 -0500

    feat: add function `assert_yields()` to `tests`

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
-from collections.abc import Callable, Iterable
+import operator
+from collections.abc import Callable, Generator, Iterable
 from typing import Any
 
 
@@ -27,3 +28,15 @@ def assert_all[T](predicate: Callable[[T], bool], items: Iterable[T], *, fail_fa
 
     if failed:
         raise AssertionError('predicate failed: ' + ', '.join(map(repr, failed)))
+
+def assert_yields[T](it: Generator[T], expected: Iterable[T], eq: Callable[[T, T], bool] = operator.eq) -> None:
+    """Asserts that ``it`` yields the items from ``expected`` and yields them in the same order.
+
+    Fails on the first item that doesn't match.
+
+    :param eq: The function to use to check if the two items are equal. Defaults to the `==` operator.
+    """
+    suffix: str = '' if eq is operator.eq else f'eq={eq!r}'
+
+    for n, (a, b) in enumerate(zip(it, expected, strict=True)):
+        assert eq(a, b), f'Item {n}: expected {b!r}, got {a!r} ({suffix})'
